3cebded236d4fce7c7936e76cb7827b3a7f624ab
Gaurav Sahal <gaurav.sahal10@gmail.com>
2022-01-08 20:15:11 +0530

    Stock buy and sell

---
+++ Arrays/Stock buy and sell.java
@@ -0,0 +1,116 @@
+/*
+Stock buy and sell 
+Medium Accuracy: 39.53% Submissions: 65466 Points: 4
+
+The cost of stock on each day is given in an array A[] of size N. Find all the days on which you buy and sell the stock so that in between those days your profit is maximum.
+Note: There may be multiple possible solutions. Return any one of them. Any correct solution will result in an output of 1, whereas wrong solutions will result in an output of 0.
+
+Example 1:
+Input:
+N = 7
+A[] = {100,180,260,310,40,535,695}
+Output:
+1
+Explanation:
+One possible solution is (0 3) (4 6)
+We can buy stock on day 0,
+and sell it on 3rd day, which will 
+give us maximum profit. Now, we buy 
+stock on day 4 and sell it on day 6.
+
+Example 2:
+Input:
+N = 5
+A[] = {4,2,2,2,4}
+Output:
+1
+Explanation:
+There are multiple possible solutions.
+one of them is (3 4)
+We can buy stock on day 3,
+and sell it on 4th day, which will 
+give us maximum profit.
+
+Your Task:
+The task is to complete the function stockBuySell() which takes an array A[] and N as input parameters and finds the days of buying and selling stock. The function must return a 2D list of integers containing all the buy-sell pairs. If there is No Profit, return an empty list.
+
+Note: Since there can be multiple solutions, the driver code will return 1 if your answer is correct, otherwise, it will return 0. In case there's no profit the driver code will return the string "No Profit" for a correct solution.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+2 ≤ N ≤ 106
+0 ≤ A[i] ≤ 106*/
+
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int A[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                A[i] = Integer.parseInt(inputLine[i]);
+            }
+            int p = 0;
+            for(int i=0; i<n-1; i++)
+                p += Math.max(0,A[i+1]-A[i]);
+                
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer> > ans = obj.stockBuySell(A, n);
+            if(ans.size()==0)
+                System.out.print("No Profit");
+            else{
+                int c=0;
+                for(int i=0; i<ans.size(); i++)
+                    c += A[ans.get(i).get(1)]-A[ans.get(i).get(0)];
+                
+                if(c==p)
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+            }System.out.println();
+        }
+    }
+}// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    //Function to find the days of buying and selling stock for max profit.
+    ArrayList<ArrayList<Integer> > stockBuySell(int A[], int n) {
+        // code here
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        int l = 0, h = 0;
+        for(int i=0; i<n-1; i++){
+            int max = Math.max(0, A[i+1]-A[i]);
+            if(max == 0){
+                if(h != 0 && h > l){
+                    ArrayList<Integer> arr = new ArrayList<>();
+                    arr.add(l);
+                    arr.add(h);
+                    list.add(arr);
+                }
+                l = i+1;
+            }
+            else
+                h = i+1;
+        }
+        if(h > l){
+            ArrayList<Integer> arr = new ArrayList<>();
+            arr.add(l);
+            arr.add(h);
+            list.add(arr);
+        }
+        return list;
+    }
+}
+
+//Total Time Taken: 1.0/2.3
