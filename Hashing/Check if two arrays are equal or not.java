8eed950e3ae899b88a9a7c20d4937dc0a42fdb25
Gaurav Sahal <gaurav.sahal10@gmail.com>
2022-03-01 21:05:38 +0530

    Check if two arrays are equal or not

---
+++ Hashing/Check if two arrays are equal or not.java
@@ -0,0 +1,93 @@
+/*
+Check if two arrays are equal or not 
+Basic Accuracy: 50.0% Submissions: 35663 Points: 1
+
+Given two arrays A and B of equal size N, the task is to find if given arrays are equal or not. Two arrays are said to be equal if both of them contain same set of elements, arrangements (or permutation) of elements may be different though.
+Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
+
+Example 1:
+Input:
+N = 5
+A[] = {1,2,5,4,0}
+B[] = {2,4,5,0,1}
+Output: 1
+Explanation: Both the array can be 
+rearranged to {0,1,2,4,5}
+
+Example 2:
+Input:
+N = 3
+A[] = {1,2,5}
+B[] = {2,4,15}
+Output: 0
+Explanation: A[] and B[] have only 
+one common value.
+
+Your Task:
+Complete check() function which takes both the given array and their size as function arguments and returns true if the arrays are equal else returns false.The 0 and 1 printing is done by the driver code.
+
+Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(N)
+
+Constraints:
+1<=N<=107
+1<=A[],B[]<=1018*/
+
+// { Driver Code Starts
+//Initial Template for Java
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");		  
+		}
+	}
+}// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+        HashMap<Long, Integer> map = new HashMap<>();
+        for(Long x : A){
+            map.put(x, map.getOrDefault(x,0)+1);
+        }
+        for(Long x : B){
+            if(map.get(x) != null)
+                map.put(x, map.get(x)-1);
+            else
+                return false;
+        }
+        for(Map.Entry<Long, Integer> e : map.entrySet()){
+            if(e.getValue() != 0)
+                return false;
+        }
+        return true;
+    }
+}
+
+//Total Time Taken: 1.9/3.2
