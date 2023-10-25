4df0dca15fc56a3923d8273b1f8ddacfe9d54411
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-10-25 23:29:56 +0530

    Remove duplicate element from sorted Linked List

---
+++ LinkedList/Remove duplicate element from sorted Linked List.java
@@ -0,0 +1,124 @@
+/*
+Remove duplicate element from sorted Linked List
+Easy Accuracy:49.37% Submissions:258K+ Points: 2
+
+Given a singly linked list consisting of N nodes. The task is to remove duplicates (nodes with duplicate values) from the given list (if exists).
+Note: Try not to use extra space. The nodes are arranged in a sorted way.
+
+Example 1:
+Input:
+LinkedList: 2->2->4->5
+Output: 2 4 5
+Explanation: In the given linked list 
+2 ->2 -> 4-> 5, only 2 occurs more 
+than 1 time. So we need to remove it once.
+
+Example 2:
+Input:
+LinkedList: 2->2->2->2->2
+Output: 2
+Explanation: In the given linked list 
+2 ->2 ->2 ->2 ->2, 2 is the only element
+and is repeated 5 times. So we need to remove
+any four 2.
+
+Your Task:
+The task is to complete the function removeDuplicates() which takes the head of input linked list as input. The function should remove the duplicates from linked list and return the head of the linkedlist.
+
+Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(1)
+
+Constraints:
+1 <= Number of nodes <= 105*/
+
+//{ Driver Code Starts
+/* package whatever; // don't place package name! */
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class Remove_Duplicate_From_LL
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        GfG g = new GfG();
+		llist.head = g.removeDuplicates(llist.head);
+		llist.printList();
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+/*
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class GfG
+{
+    //Function to remove duplicates from sorted linked list.
+    Node removeDuplicates(Node head)
+    {
+	    // Your code here	
+	    Node curr = head;
+	    while(curr.next != null) {
+	        if(curr.data == curr.next.data) {
+	            curr.next = curr.next.next;
+	        }
+	        else {
+	            curr = curr.next;
+	        }
+	    }
+	    return head;
+    }
+}
